d82745fa79a737003b3a4f4bc2836f2a39180a48
mdYeasin-cmd <yeasin200011@gmail.com>
2022-11-08 14:39:52 +0600

    Database connected and a post api created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,30 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Database
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0nieed1.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const foodCollection = client.db('foodsInfoDB').collection('foods');
+        // Create Operation
+        app.post('/addNewFood', async(req, res) => {
+            const foodItem = req.body;
+            console.log(foodItem);
+            const result = await foodCollection.insertOne(foodItem);
+            res.send(result);
+        })
+    }
+    catch {
+
+    }
+}
+
+run().catch(error => console.error(error));
 
 // Server testing API
 app.get('/', (req, res) => {
